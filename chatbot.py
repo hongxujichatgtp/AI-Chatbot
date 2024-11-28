b11311aca8fe0f39d373658570c9711dee832dad
hongxujichatgtp <hongxujichatgtp@gmail.com>
2024-11-28 09:45:02 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,11 +3,15 @@ import random
 responses = {
     'hello': 'Hi there!',
     'how are you': 'I am good, how about you?',
-    'bye': 'Goodbye! Have a nice day!'
+    'bye': 'Goodbye! Have a nice day!',
+    'joke': ['Why don’t skeletons fight each other? They don’t have the guts.', 
+             'I told my wife she was drawing her eyebrows too high. She looked surprised.']
 }
 
 def chatbot_response(user_input):
     user_input = user_input.lower()
+    if user_input == 'joke':
+        return random.choice(responses[user_input])
     return responses.get(user_input, "Sorry, I don't understand.")
 
 while True:
